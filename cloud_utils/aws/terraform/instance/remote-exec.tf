da180d1d1748a0ed3236d44be075aa0a96afbc9d
tdaly61 <tdaly61@gmail.com>
2022-06-20 19:57:11 +0930

    working provisioner

--- cloud_utils/aws/terraform/instance/remote-exec.tf
+++ cloud_utils/aws/terraform/instance/remote-exec.tf
@@ -3,16 +3,18 @@ resource "null_resource" "remote-exec" {
     provisioner "remote-exec" {
       connection {
         agent = false
-        timeout = "30m"
-        host = "aws_instance.mini-loop-1"
+        timeout = "5m"
+        host = "${aws_instance.mini-loop-1.public_ip}"
         user = "ubuntu"
-        private_key = file("/home/aws/.ssh/tdaly-user.pem")
+        private_key = "${file("/home/aws/.ssh/tdaly-user.pem")}" 
     }
       inline = [
         "touch ~/IMadeAFile.Right.Here",
         "git clone https://github.com/tdaly61/mini-loop.git",
         "cd mini-loop; git fetch origin k3s22",
         "cd mini-loop; git checkout k3s22",
+        "cd mini-loop; git config --global user.name tdaly61", 
+        "cd mini-loop; git config --global user.email tdaly61@gmail.com", 
       ]
     }
 }
